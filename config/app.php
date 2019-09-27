a11a288dc17bc2bfabca4ca8239211b19556b9d3
lixiuxiong <xiuxiong.li@teamtopgame.com>
2019-09-27 16:57:21 +0800

    feat(horizon):注册HorizonServiceProvider

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \App\Providers\HorizonServiceProvider::class,
 
         /*
          * Application Service Providers...
